35e35a1422c7ff7ac45f7d354ebbd77dec96f609
Pete Walker <pete.walker@me.com>
2018-07-23 18:08:53 +0100

    Fixes openactive/data-model-validator#58 - adds basic models and basic filtering

--- src/client/components/Results.jsx
+++ src/client/components/Results.jsx
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Pluralize from 'react-pluralize'
 
 export default class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.severities = {
+      failure: {
+        name: 'Error',
+        icon: 'times',
+      },
+      warning: {
+        name: 'Warning',
+        icon: 'exclamation',
+      },
+      notice: {
+        name: 'Tip',
+        icon: 'check',
+      },
+    };
+    const show = {};
+    for (const severity in this.severities) {
+      show[severity] = true;
+    }
+    this.state = { show };
+  }
+
   static fieldName(path) {
     const elements = path.split('.');
     return elements[elements.length - 1];
@@ -12,28 +37,86 @@ export default class Results extends Component {
     }
   }
 
+  toggleSeverity(severity) {
+    const stateShow = Object.assign(
+      {},
+      this.state.show,
+      {
+        [severity]: !this.state.show[severity],
+      },
+    );
+    this.setState({ show: stateShow });
+    console.log(stateShow);
+  }
+
   render() {
     if (this.props.results) {
-      const resultList = this.props.results.map(
-        (result, index) => {
-          const fieldName = this.constructor.fieldName(result.path);
-          return (
-            <li key={index} className={result.severity} onClick={() => this.handleClick(result.path)}>
-              <span className="result-title">{result.severity}</span>
-              <div className="row">
-                <div className="col-4">
-                  <span title={result.path} className="result-field">{fieldName}</span>
-                </div>
-                <div className="col-8">
-                  <span className="result-message">{result.message}</span>
-                </div>
-              </div>
-            </li>
+
+
+      const resultList = {};
+      const counts = {};
+      const buttonList = [];
+      const listList = [];
+
+      for (const severity in this.severities) {
+        counts[severity] = 0;
+        resultList[severity] = this.props.results.map(
+          (result, index) => {
+            const fieldName = this.constructor.fieldName(result.path);
+            if (result.severity === severity) {
+              counts[severity] += 1;
+              return (
+                <li key={index} className={result.severity} onClick={() => this.handleClick(result.path)}>
+                  <span className="result-title">{result.severity}</span>
+                  <div className="row">
+                    <div className="col-4">
+                      <span title={result.path} className="result-field">{fieldName}</span>
+                    </div>
+                    <div className="col-8">
+                      <span className="result-message">{result.message}</span>
+                    </div>
+                  </div>
+                </li>
+              );
+            }
+            return null;
+          },
+        );
+        buttonList.push(
+          (
+            <button
+              key={"btn-" + severity}
+              type="button"
+              className={"btn button-primary--alt " + (this.state.show[severity] ? "active" : "") }
+              disabled={counts[severity] === 0}
+              onClick={() => this.toggleSeverity(severity)}
+              >
+              <FontAwesomeIcon fixedWidth={true} icon={this.severities[severity].icon} />
+              <Pluralize singular={this.severities[severity].name} count={counts[severity]} />
+            </button>
+          )
+        );
+        if (this.state.show[severity]) {
+          listList.push(
+            (
+              <ul
+                key={"list-" + severity}
+                className={"result-list " + severity + "-list"}
+                >
+                  {resultList[severity]}
+              </ul>
+            )
           );
-        },
-      );
+        }
+      }
+
       return (
-        <ul className="result-list">{resultList}</ul>
+        <div className="results">
+          <div className="btn-group">
+            {buttonList}
+          </div>
+          {listList}
+        </div>
       );
     }
     return '';
