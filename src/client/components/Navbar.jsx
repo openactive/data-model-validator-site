10a7ee9b70e0fc915f0a72c93dc34f29839ae659
Pete Walker <pete.walker@me.com>
2018-07-25 12:11:34 +0100

    Fixes openactive/data-model-validator#69 - aligns header with the developer microsite

--- src/client/components/Navbar.jsx
+++ src/client/components/Navbar.jsx
@@ -5,12 +5,20 @@ export default class Navbar extends Component {
   /* eslint-disable class-methods-use-this */
   render() {
     return (
-      <nav className="navbar navbar-expand-md fixed-top">
-        <a className="navbar-brand" href="#"><LogoSVG width={80} height={52}/></a>
-        <div className="information-row text-center">
-          <p>
-            This tool allows you to validate your data models against the <a href="https://www.openactive.io/modelling-opportunity-data/" target="_blank">Modelling Opportunity Data Specification</a>.
-          </p>
+      <nav className="navbar navbar-expand-md navbar-light fixed-top">
+        <a className="navbar-brand" href="/"><LogoSVG width={80} height={52}/></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="https://developer.openactive.io/">Developer reference</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="https://www.openactive.io/">Open your data</a>
+            </li>
+          </ul>
         </div>
       </nav>
     );
