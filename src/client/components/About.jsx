4fd94860c0caed0cef767cb3d93c737d884d728c
Pete Walker <pete.walker@me.com>
2018-09-03 11:10:58 +0100

    Fixes openactive/data-model-validator#224
    Adds the RPDE rules to the about page

--- src/client/components/About.jsx
+++ src/client/components/About.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { defaultRules } from '@openactive/data-model-validator';
+import { defaultRules as modelRules } from '@openactive/data-model-validator';
+import { defaultRules as rpdeRules } from '@openactive/rpde-validator';
 
 import VersionHelper from '../helpers/version-helper';
 import consts from '../data/consts';
@@ -23,62 +24,77 @@ export default class About extends Component {
   render() {
     const versions = VersionHelper.getUniqueVersions();
     let version = '';
+    let specUrl = consts.MODELLING_SPECIFICATION_URL;
     if (versions.length === 1) {
       version = ` v${versions[0]}`;
+      const meta = VersionHelper.getVersionMetaData(versions[0]);
+      specUrl = { meta };
     }
 
-    const rulesList = [];
-    const defaultRulesConcat = [...defaultRules.raw, ...defaultRules.core];
-    for (let index = 0; index < defaultRulesConcat.length; index += 1) {
-      const rule = new defaultRulesConcat[index]();
-      const testList = [];
-      for (const testKey in rule.meta.tests) {
-        if (Object.prototype.hasOwnProperty.call(rule.meta.tests, testKey)) {
-          const test = rule.meta.tests[testKey];
-          let { message } = test;
-          if (typeof test.sampleValues !== 'undefined') {
-            for (const key in test.sampleValues) {
-              if (Object.prototype.hasOwnProperty.call(test.sampleValues, key)) {
-                message = message.replace(new RegExp(`{{${key}}}`, 'g'), test.sampleValues[key]);
+    const rulesList = {};
+    const modelRulesConcat = [...modelRules.raw, ...modelRules.core];
+    const rpdeRulesConcat = [...rpdeRules.http, ...rpdeRules.page, ...rpdeRules.preLastPage, ...rpdeRules.lastPage];
+    const ruleObj = {
+      model: modelRulesConcat,
+      rpde: rpdeRulesConcat,
+    };
+    for (const key in ruleObj) {
+      if (Object.prototype.hasOwnProperty.call(ruleObj, key)) {
+        const rules = ruleObj[key];
+        rulesList[key] = [];
+        for (let index = 0; index < rules.length; index += 1) {
+          const rule = new rules[index]();
+          const testList = [];
+          for (const testKey in rule.meta.tests) {
+            if (Object.prototype.hasOwnProperty.call(rule.meta.tests, testKey)) {
+              const test = rule.meta.tests[testKey];
+              let { message } = test;
+              if (typeof test.sampleValues !== 'undefined') {
+                for (const sampleKey in test.sampleValues) {
+                  if (Object.prototype.hasOwnProperty.call(test.sampleValues, sampleKey)) {
+                    message = message.replace(new RegExp(`{{${sampleKey}}}`, 'g'), test.sampleValues[sampleKey]);
+                  }
+                }
               }
+              testList.push(
+                <tr key={`tr-${rule.meta.name}-${testKey}`}>
+                  <td width="15%" className="table-primary">
+                    {this.categories[test.category].name}
+                  </td>
+                  <td width="15%" className={`td-severity td-severity-${test.severity}`}>
+                    {test.severity}
+                  </td>
+                  <td>
+                    <p>{test.description || rule.meta.description}</p>
+                    <h6>Example message</h6>
+                    <blockquote>
+                      <p>{message}</p>
+                    </blockquote>
+                  </td>
+                </tr>,
+              );
             }
           }
-          testList.push(
-            <tr key={`tr-${rule.meta.name}-${testKey}`}>
-              <td width="15%" className="table-primary">
-                {this.categories[test.category].name}
-              </td>
-              <td width="15%" className={`td-severity td-severity-${test.severity}`}>
-                {test.severity}
-              </td>
-              <td>
-                <p>{test.description || rule.meta.description}</p>
-                <h6>Example message</h6>
-                <blockquote>
-                  <p>{message}</p>
-                </blockquote>
-              </td>
-            </tr>,
+          rulesList[key].push(
+            <div key={`rule-${rule.meta.name}`} className="rule-definition">
+              <h5>{rule.meta.name}</h5>
+              <table className="table table-bordered" width="100%">
+                <tbody>
+                  {testList}
+                </tbody>
+              </table>
+            </div>,
           );
         }
       }
-      rulesList.push(
-        <div key={`rule-${rule.meta.name}`} className="rule-definition">
-          <h5>{rule.meta.name}</h5>
-          <table className="table table-bordered" width="100%">
-            <tbody>
-              {testList}
-            </tbody>
-          </table>
-        </div>,
-      );
     }
+
     return (
       <div className="about-page">
         <div className="jumbotron">
           <div className="container">
             <h1 className="display-3">OpenActive Data Validator</h1>
-            <p><a href="https://www.openactive.io/">OpenActive</a> is a community-led initiative using open data to help people get active. This validator allows you to validate a JSON document against the <a href={consts.MODELLING_SPECIFICATION_URL}>Modelling Opportunity Specification{version}</a>.</p>
+            <p><a href="https://www.openactive.io/">OpenActive</a> is a community-led initiative using open data to help people get active. This validator allows you to validate a JSON document against the <a href={specUrl}>Modelling Opportunity Specification{version}</a>.</p>
           </div>
         </div>
         <div className="container">
@@ -87,18 +103,34 @@ export default class About extends Component {
               <h4>Limitations</h4>
               <p>This validator will currently <strong>NOT:</strong></p>
               <ul>
-                <li>Fully validate RPDE feeds, although it can validate the data models within a valid feed</li>
                 <li>Automatically fix bad data</li>
                 <li>Validate custom properties</li>
-                <li>Validate properties in schema.org that are not in the <a href={consts.MODELLING_SPECIFICATION_URL}>Modelling Opportunity Specification{version}</a>.</li>
+                <li>Validate properties in schema.org that are not in the <a href={specUrl}>Modelling Opportunity Specification{version}</a>.</li>
               </ul>
             </div>
           </div>
           <div className="row">
             <div className="col">
               <h4>Rules</h4>
-              <p>The validator applies the following rules:</p>
-              {rulesList}
+              <p>Below you can view the rules and sample messages applied by each validator:</p>
+              <ul>
+                <li><a href="#modelRules">Model validation rules</a></li>
+                <li><a href="#rpdeRules">RPDE feed validation rules</a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h4 id="modelRules">Model validation rules</h4>
+              <p>The model validator applies the following rules:</p>
+              {rulesList.model}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h4 id="rpdeRules">RPDE validation rules</h4>
+              <p>The RPDE feed validator applies the following rules:</p>
+              {rulesList.rpde}
             </div>
           </div>
         </div>
