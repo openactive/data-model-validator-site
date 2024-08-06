aafbf14c419c077fd9d412b2fa2734d807d42bc9
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2024-08-06 20:58:03 +0100

    chore: Add Lost Pixel support

---
+++ lostpixel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pageShots: {
+    pages: [
+      { path: '/', name: 'model-validator-home' },
+      { path: '/rpde', name: 'rpde-validator-home' },
+      { path: '/?url=https%3A%2F%2Fopenactive.io%2Fdata-models%2Fversions%2F2.x%2Fexamples%2Fsessionseries-split_example_1.json&version=2.x', name: 'sessionseries' },
+      { path: '/?url=https%3A%2F%2Fopenactive.io%2Fdata-models%2Fversions%2F2.x%2Fexamples%2Fscheduledsession-split_example_1.json&version=2.x', name: 'scheduledsession' },
+    ],
+    baseUrl: 'http://localhost:8080',
+  },
+  lostPixelProjectId: 'clzitt4s52b6dlcoguswodrsj',
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
