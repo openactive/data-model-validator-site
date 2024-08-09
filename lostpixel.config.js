1bba4124cfd98b1c447c03d764609ae45f2486ae
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2024-08-09 15:17:44 +0100

    chore: Add smoke test to CI deployment

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -6,9 +6,11 @@ module.exports = {
       { path: '/?url=https%3A%2F%2Fopenactive.io%2Fdata-models%2Fversions%2F2.x%2Fexamples%2Fsessionseries-split_example_1.json&version=2.x', name: 'sessionseries' },
       { path: '/?url=https%3A%2F%2Fopenactive.io%2Fdata-models%2Fversions%2F2.x%2Fexamples%2Fscheduledsession-split_example_1.json&version=2.x', name: 'scheduledsession' },
     ],
-    baseUrl: 'http://172.17.0.1:8080',
+    baseUrl: process.env.LOST_PIXEL_BASE_URL || 'http://172.17.0.1:8080',
   },
   lostPixelProjectId: 'clzitt4s52b6dlcoguswodrsj',
-  failOnDifference: true,
+  failOnDifference: process.env.LOST_PIXEL_FAIL_ON_DIFFERENCE === 'true' || false,
+  threshold: 0,
+  setPendingStatusCheck: process.env.LOST_PIXEL_FAIL_ON_DIFFERENCE !== 'true',
   apiKey: process.env.LOST_PIXEL_API_KEY,
 };
